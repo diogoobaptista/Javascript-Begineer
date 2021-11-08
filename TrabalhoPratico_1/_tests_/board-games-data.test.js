9d02800d49b8db313a7e2a222d13d1f3b0ea6f2f
Diogo <diogomsbaptista@gmail.com>
2021-11-08 18:06:12 +0000

    fix

--- TrabalhoPratico_1/_tests_/board-games-data.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-'use strict'
-
-const { getBody } = require('../board-games-data')
-
-
-test('Test json files', () => {
-    getBody('TrabalhoPratico_1/docs/gameIdsList.txt')
-})
